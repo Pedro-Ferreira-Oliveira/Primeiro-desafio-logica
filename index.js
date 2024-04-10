6dca50bd77dd868e4c5024816313824db1f0ff33
Pedro Ferreira <116686587+Pedro-Ferreira-Oliveira@users.noreply.github.com>
2024-04-10 20:24:42 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+//Mensagem de bem-vindo
+console.log( " Seja bem vindo treinador " )
+
+
+//codigo da equipe pokemom
+let timepokemom = [
+[ " Poochyena " , 2 , " M " , 13 , 13 ],
+[ " Zigzagoon " , 2 , " F " , 13 , 13 ],
+[ " Dragonite " , 5 , " M " , 25 , 25 ],
+[ " Dragonite " , 5 , " F " , 24 , 24 ],
+[ " Dragonite " , 5 , " F " , 24 , 24 ],
+[ " Poochyena " , 3 , " F " , 15 , 15 ],
+[ " Wurmple " , 2 , " M " , 7 , 14]] 
+console.log( " O seu: " + timepokemom[0][0] + " - LvL: " + timepokemom[0][1] + " - Possui o sexo: " + timepokemom[0][2] + " - HP atual: " + timepokemom[0][3] + " - HP total: " + timepokemom[0][4] + " Pokemom cadastrado com sucesso ")
+console.log( " O seu: " + timepokemom[1][0] + " - LvL: " + timepokemom[1][1] + " - Possui o sexo: " + timepokemom[1][2] + " - HP atual: " + timepokemom[1][3] + " - HP total: " + timepokemom[1][4] + " Pokemom cadastrado com sucesso ")
+console.log( " O seu: " + timepokemom[2][0] + " - LvL: " + timepokemom[2][1] + " - Possui o sexo: " + timepokemom[2][2] + " - HP atual: " + timepokemom[2][3] + " - HP total: " + timepokemom[2][4] + " Pokemom cadastrado com sucesso ")
+console.log( " O seu: " + timepokemom[3][0] + " - LvL: " + timepokemom[3][1] + " - Possui o sexo: " + timepokemom[3][2] + " - HP atual: " + timepokemom[3][3] + " - HP total: " + timepokemom[3][4] + " Pokemom cadastrado com sucesso ")
+console.log( " O seu: " + timepokemom[4][0] + " - LvL: " + timepokemom[4][1] + " - Possui o sexo: " + timepokemom[4][2] + " - HP atual: " + timepokemom[4][3] + " - HP total: " + timepokemom[4][4] + " Pokemom cadastrado com sucesso ")
+console.log( " O seu: " + timepokemom[5][0] + " - LvL: " + timepokemom[5][1] + " - Possui o sexo: " + timepokemom[5][2] + " - HP atual: " + timepokemom[5][3] + " - HP total: " + timepokemom[5][4] + " Pokemom cadastrado com sucesso ")
+console.log( " O seu: " + timepokemom[6][0] + " - LvL: " + timepokemom[6][1] + " - Possui o sexo: " + timepokemom[6][2] + " - HP atual: " + timepokemom[6][3] + " - HP total: " + timepokemom[6][4] + " Pokemom cadastrado com sucesso ")
